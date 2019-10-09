58afd135a411c808d17ffbfc608d4893996b452a
Kaiming Ouyang <kouya001@ucr.edu>
2019-10-09 10:41:04 -0500

    delete warm-up cg

--- src/driver.f
+++ src/driver.f
@@ -51,7 +51,7 @@ c     SET UP and RUN NEKBONE
            call set_f(f,c,n)
 
            if(nid.eq.0) write(6,*)
-           call cg(x,f,g,c,r,w,p,z,n,niter,flop_cg)
+           !call cg(x,f,g,c,r,w,p,z,n,niter,flop_cg)
 
            call nekgsync()
 
